7863f33edb5342faa58c31d177c36a9e44ed03c4
methreals <95840752+methreals@users.noreply.github.com>
2023-05-27 10:14:25 +0800

    added # into / route

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/users', userRouter);
 
 app.get('/', (req, res) => {
     const reqQuery = req.query;
-    console.log('req.query', req.query)
+    console.log('#### req.query', req.query)
     res.send('Hello World!');
 });
 
