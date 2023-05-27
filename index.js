e2d66df454bef25632943b306e1e5f5be5899191
jeff cheung <jeffcheung2019@gmail.com>
2023-05-27 10:51:53 +0800

    updated version number 1.0.1

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userRouter = require("./src/routers/user")
+const userRouter = require("./src/routers/user");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use('/users', userRouter);
+app.use("/users", userRouter);
 
-app.get('/version', (req, res) => {
-    res.send('version : 1.0.0');
+app.get("/version", (req, res) => {
+  res.send("version : 1.0.1");
 });
 
-app.get('/', (req, res) => {
-    const reqQuery = req.query;
-    console.log('#### req.query', req.query)
-    res.send('Hello World!');
+app.get("/", (req, res) => {
+  const reqQuery = req.query;
+  console.log("#### req.query", req.query);
+  res.send("Hello World!");
 });
 
-app.post('/submit', (req, res) => {
-    const reqBodyMsg = req.body.message;
-    res.send(`submitted info : ${reqBodyMsg}`)
-})
+app.post("/submit", (req, res) => {
+  const reqBodyMsg = req.body.message;
+  res.send(`submitted info : ${reqBodyMsg}`);
+});
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
